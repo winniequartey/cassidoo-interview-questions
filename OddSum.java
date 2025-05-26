0279d1a4252b0c1ac1c20981c96576d8d035cb21
winniequartey <8668847+winniequartey@users.noreply.github.com>
2025-05-26 11:11:12 +0000

    stored pairs in array instead of map

--- OddSum.java
+++ OddSum.java
@@ -2,9 +2,7 @@
  * Cassidoo Interview question of the week @ https://buttondown.com/cassidoo/archive/false-expectations-take-away-joy-sandra-bullock/
  */
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 public class OddSum {
     public static void main(String[] args){
@@ -12,24 +10,27 @@ public class OddSum {
         System.out.println(getOddSumPair(new int[] {2, 4, 6, 8}, new int[] {10, 12, 14}));
     }
 
-    public static List<Map<Integer, Integer>>  getOddSumPair(int[] arr1, int[] arr2){
-        List<Map<Integer, Integer>> oddSumPairs = new ArrayList<>();
-        if(null == arr1 || null == arr2){
+    public static List<List>  getOddSumPair(int[] arr1, int[] arr2){
+        List<List> oddSumPairs = new ArrayList<>();
+        if (null == arr1 || null == arr2){
             return oddSumPairs;
         }
-        if(arr1.length < 1 || arr2.length < 1){
+        if (arr1.length < 1 || arr2.length < 1){
             return oddSumPairs;
         }
-        for(int num1 : arr1){
-            for(int num2 : arr2){
-                if((num1 % 2 == 0) && (num2 % 2 == 0)){
+        for (int num1 : arr1){
+            for (int num2 : arr2){
+                if ((num1 % 2 == 0) && (num2 % 2 == 0)) {
                     continue;
                 }
-                else if((num1 % 2 != 0) && (num2 % 2 != 0)){
+                else if ((num1 % 2 != 0) && (num2 % 2 != 0)) {
                     continue;
                 }
-                else{
-                    oddSumPairs.add(Collections.singletonMap(num1, num2));
+                else {
+                    List<Integer> pair = new ArrayList<>();
+                    pair.add(num1);
+                    pair.add(num2);
+                    oddSumPairs.add(pair);
                 }
             }
         }
